85e6e9d350899499bc43101694999b215311f928
茗少 <1753972608@qq.com>
2020-10-20 11:34:23 +0800

    细节修改

--- src/main/java/maimai_new/demo/controller/PageController.java
+++ src/main/java/maimai_new/demo/controller/PageController.java
@@ -1,4 +1,15 @@
 package maimai_new.demo.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class PageController {
+
+
+    @RequestMapping("/test_login")
+    public String login(){
+        return "login";
+    }
 }
