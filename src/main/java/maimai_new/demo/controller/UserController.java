8dbf5d03c3f48445a7e3e3acf3c532949676774f
茗少 <1753972608@qq.com>
2020-10-27 17:25:39 +0800

    细节修改

--- src/main/java/maimai_new/demo/controller/UserController.java
+++ src/main/java/maimai_new/demo/controller/UserController.java
@@ -1,13 +1,17 @@
 package maimai_new.demo.controller;
 
 
+import maimai_new.demo.dao.SessionInfo;
 import maimai_new.demo.dao.rand.randomUtils;
 import maimai_new.demo.impl.UserServiceImpl;
 import maimai_new.demo.impl.aliyunUtils.AliYun;
+import maimai_new.demo.impl.redisUtils.RedisServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class UserController{
@@ -16,6 +20,17 @@ public class UserController{
     @Autowired
     private UserServiceImpl userService;
 
+    @Autowired
+    private RedisServiceImpl redisService;
+
+
+    /**
+     *
+     * @param phone
+     * @param randUserId
+     * @return
+     * 发送手机验证码
+     */
     @ResponseBody
     @RequestMapping("/sendPhoneCode")
     public int sendPhoneCode(String phone,String randUserId){
@@ -25,6 +40,7 @@ public class UserController{
         try {
             String code=randomUtils.getPhoneCode();
             AliYun.sendCode(phone, code);
+            redisService.savePhoneCode(randUserId,code);
             System.out.println("验证码为:"+code);
         }catch (Exception e){
             e.printStackTrace();
@@ -34,5 +50,41 @@ public class UserController{
     }
 
 
+    /**
+     *
+     * @param phone
+     * @param rand_uuid
+     * @param code
+     * @param request
+     * @return
+     * 校验验证码
+     */
+    @ResponseBody
+    @RequestMapping("/checkPhoneCode")
+    public int checkPhoneCode(String phone,
+                              String rand_uuid,
+                              String code,
+                              HttpServletRequest request){
+        if(redisService.getPhoneCode(rand_uuid).equals(code)){
+            HttpSession session=request.getSession();
+            session.setAttribute(SessionInfo.USER_ID,phone);
+            return 1;
+        }
+        return 0;
+    }
 
+
+
+    /**
+     *
+     * @param request
+     * 退出登录
+     */
+    @ResponseBody
+    @RequestMapping("/logOut")
+    public int logOut(HttpServletRequest request){
+        HttpSession session=request.getSession();
+        session.removeAttribute(SessionInfo.USER_ID);
+        return 1;
+    }
 }
---
+++ src/main/java/maimai_new/demo/dao/SessionInfo.java
@@ -0,0 +1,6 @@
+package maimai_new.demo.dao;
+
+public class SessionInfo {
+
+    public static String USER_ID="User_id";
+}
